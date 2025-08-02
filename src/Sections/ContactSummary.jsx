8f8b4bedae2b3bd53bb9e58d3506c9945661d259
Taran Kumar <140129581+tarankumar001@users.noreply.github.com>
2025-08-02 22:49:22 +0530

    implemented marquuee

--- src/Sections/ContactSummary.jsx
+++ src/Sections/ContactSummary.jsx
@@ -1,20 +1,58 @@
-import { useRef } from "react"
+import { useRef } from "react";
+import Marquee from "../components/Marquee";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
- 
+gsap.registerPlugin(ScrollTrigger);
 
 const ContactSummary = () => {
-    const containerRef=useRef(null)
-  return (
-<section ref={containerRef} className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16">
-    {/* Marquee */}
-    <div className="overflow-hidden font-light text-center contact-text-responsive">
-        <p>"Lets build a <br/>
-        <span className="font-normal">memorable</span>&{" "} <span className="italic">inspiring</span><br/>
-        web application<span className="text-gold">togeher</span>"</p>
-    </div>
-    {/* Marquee */}
-</section>  
-)
-}
+  const containerRef = useRef(null);
+  const items = ["Innovation", "Precision", "Trust", "Collaboration", "Excellence"];
+  const items2 = ["contact me", "contact me", "contact me", "contact me", "contact me"];
 
-export default ContactSummary
+  useGSAP(() => {
+    gsap.to(containerRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+        markers: false,
+      },
+    });
+  }, []);
+
+  return (
+    <section
+      ref={containerRef}
+      className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16"
+    >
+      {/* Marquee */}
+      <Marquee items={items} />
+
+      <div className="overflow-hidden font-light text-center contact-text-responsive">
+        <p>
+          "Let's build a <br />
+          <span className="font-normal">memorable</span>&nbsp;
+          <span className="italic">inspiring</span>
+          <br />
+          web application <span className="text-gold">together</span>"
+        </p>
+      </div>
+
+      {/* Marquee */}
+      <Marquee
+        items={items2}
+        reverse={true}
+        className="text-black bg-transparent border-2"
+        iconClassName="stroke-gold stroke-2 text-primary"
+        icon="material-symbols-light:square"
+      />
+    </section>
+  );
+};
+
+export default ContactSummary;
